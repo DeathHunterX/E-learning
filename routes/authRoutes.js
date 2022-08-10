d8055e40f0219a64721cad69b887ba65ef3669a1
Phan Thanh Loi <phanthanhloi22112001@gmail.com>
2022-08-10 12:57:06 +0700

    first commit

---
+++ routes/authRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router()
+const authCtrl = require('../controllers/authCtrl')
+
+const auth = require('../middleware/auth')
+const authAdmin = require('../middleware/adminAuth')
+
+router.post('/register', authCtrl.register)
+
+router.post('/login', authCtrl.login)
+
+router.post('/logout', authCtrl.logout)
+
+router.post('/refresh_token', authCtrl.getAccessToken)
+
+router.post('/reset', auth, authCtrl.resetPassword)
+
+module.exports = router
